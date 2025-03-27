b027b6732ed50bae007d6c832de70acd304c7ab5
edwardkuo09 <edwardkuo0909@gmail.com>
2025-03-27 18:12:03 +0800

    lab4 finish

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -7,16 +7,28 @@ const puppeteer = require('puppeteer');
 
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
-
+    // sometimes fault but I don't know why.
     // Hints:
     // Click search button
+    await page.waitForSelector('#__docusaurus > nav > div.navbar__inner > div.navbar__items.navbar__items--right > div.navbarSearchContainer_IP3a > button');
+  
+    await page.click('#__docusaurus > nav > div.navbar__inner > div.navbar__items.navbar__items--right > div.navbarSearchContainer_IP3a > button');
     // Type into search box
+    await page.waitForSelector('#docsearch-input');
+  
+    await page.type('#docsearch-input','andy popoo');
     // Wait for search result
+    await page.waitForSelector('body > div:nth-child(1) > div > div > div > div');
+    
     // Get the `Docs` result section
+    await page.waitForSelector('#docsearch-hits1-item-4 > a > div');
     // Click on first result in `Docs` section
+    await page.click('#docsearch-hits1-item-4 > a > div');
     // Locate the title
+    const textSelector = await page.waitForSelector('#__docusaurus_skipToContent_fallback > div > div > main > div > div > div > div > article > div.theme-doc-markdown.markdown > header > h1');
+    const fultitle = await textSelector ?.evaluate(el =>el.textContent);
     // Print the title
-
+    console.log(fultitle);
     // Close the browser
     await browser.close();
 })();

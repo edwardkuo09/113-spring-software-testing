e3b986821125fe618f26bcaa95d1840149b8a019
edwardkuo09 <edwardkuo0909@gmail.com>
2025-04-27 22:07:41 +0800

    lab5 finish

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,13 @@
 #include <string.h>
+#include <stdio.h>
 
 void antiasan(unsigned long addr)
 {
+	// printf(" %p\n",(void *)((addr - 0x100)>>3));
+	// printf(" %p\n",(void *)(((addr - 0x100)>>3)+0x7fff8000));
+	// shift to shadow mem in AddressSanitizer
+	unsigned char *s = (unsigned char*)(((addr)>>3)+0x7fff8000);
+	unsigned char *e = (unsigned char*)(((addr+0x100) >> 3) + 0x7fff8000);
 
+	while(s < e) *s++ = 0;
 }

98642edd5a87352f8577b65487add9eaa543764d
edwardkuo09 <edwardkuo0909@gmail.com>
2025-05-15 22:23:57 +0800

    lab8

--- lab8/solve.py
+++ lab8/solve.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 
 import angr,sys
+import claripy
 
 def main():
-    secret_key = b""
+    project = angr.Project("./chal", auto_load_libs=False)
+    input_size = 8
+    input_chars = [claripy.BVS(f'input_{i}', 8) for i in range(input_size)]
+    input_concat = claripy.Concat(*input_chars+[claripy.BVV(0, 8)])
+    state = project.factory.full_init_state(
+        args=["./chal"],
+        stdin=input_concat,
+    )
+    sm = project.factory.simulation_manager(state)
+    sm.explore(find=0x401307)
+    found = sm.found[0]
+    secret_key = found.solver.eval(input_concat, cast_to=bytes)
+    secret_key = secret_key[:secret_key.find(b"\x00")]
     sys.stdout.buffer.write(secret_key)
-
+    
 
 if __name__ == '__main__':
     main()
